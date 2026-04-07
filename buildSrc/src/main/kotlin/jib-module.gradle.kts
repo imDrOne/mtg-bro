7c05f2702e69bfe4784fafa1fce011823f45bac8
Andrey Tikholoz <idronecoder@gmail.com>
2026-04-07 22:47:27 +0300

    fetaure/init-ci: fix

--- buildSrc/src/main/kotlin/jib-module.gradle.kts
+++ buildSrc/src/main/kotlin/jib-module.gradle.kts
@@ -16,6 +16,16 @@ val dockerExecutable: String by lazy {
 jib {
     from {
         image = "eclipse-temurin:21-jre-alpine"
+        platforms {
+            platform {
+                architecture = "amd64"
+                os = "linux"
+            }
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
     }
     to {
         image = "mtg-bro/${project.name}:latest"
