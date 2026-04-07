f534119c5f827ed28e1535ae68a510db9d0ed948
Andrey Tikholoz <idronecoder@gmail.com>
2026-04-07 22:18:24 +0300

    fetaure/init-ci: Install postgresql driver for liquibase

--- docker/postgres/init-databases.sh
+++ docker/postgres/init-databases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
     CREATE DATABASE collection_manager_db;
     GRANT ALL PRIVILEGES ON DATABASE collection_manager_db TO $POSTGRES_USER;
 
