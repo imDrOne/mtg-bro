0292c35630b60ae177be0ce74cdee6628d3cd14b
Andrey Tikholoz <idronecoder@gmail.com>
2026-04-07 20:00:33 +0300

    fetaure/init-ci: Implement basic ci/cd

---
+++ docker/postgres/init-databases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE collection_manager_db;
+    GRANT ALL PRIVILEGES ON DATABASE collection_manager_db TO $POSTGRES_USER;
+
+    CREATE DATABASE draftsim_parser_db;
+    GRANT ALL PRIVILEGES ON DATABASE draftsim_parser_db TO $POSTGRES_USER;
+
+    CREATE DATABASE wizard_stat_db;
+    GRANT ALL PRIVILEGES ON DATABASE wizard_stat_db TO $POSTGRES_USER;
+EOSQL
