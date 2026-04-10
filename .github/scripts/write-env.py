e6414c64b313033384f062805abf4d0e459fdb68
Andrey Tikholoz <idronecoder@gmail.com>
2026-04-10 22:51:37 +0300

    fetaure/init-ci: Fix python script;

--- .github/scripts/write-env.py
+++ .github/scripts/write-env.py
@@ -7,9 +7,9 @@ import os
 
 secrets = json.loads(os.environ["ALL_SECRETS"])
 
-CI_SECRETS = {"GITHUB_TOKEN", "SSH_HOST", "SSH_USER", "SSH_PRIVATE_KEY", "GHCR_TOKEN"}
+CI_SECRETS = {"github_token", "ssh_host", "ssh_user", "ssh_private_key", "ghcr_token"}
 
-lines = "\n".join(f"{k}={v}" for k, v in secrets.items() if k not in CI_SECRETS)
+lines = "\n".join(f"{k}={v}" for k, v in secrets.items() if k.lower() not in CI_SECRETS)
 
 with open("/tmp/module.env", "w") as f:
     f.write(lines + "\n")
