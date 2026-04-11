77f73cd42503c419768cd9d47cf18837ade97014
Andrey Tikholoz <idronecoder@gmail.com>
2026-04-11 14:43:03 +0300

    fetaure/init-ci: Fix init db script;

--- docker/postgres/init-databases.sh
+++ docker/postgres/init-databases.sh
@@ -3,11 +3,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
     CREATE DATABASE collection_manager_db;
-    GRANT ALL PRIVILEGES ON DATABASE collection_manager_db TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE collection_manager_db TO "$POSTGRES_USER";
 
     CREATE DATABASE draftsim_parser_db;
-    GRANT ALL PRIVILEGES ON DATABASE draftsim_parser_db TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE draftsim_parser_db TO "$POSTGRES_USER";
 
     CREATE DATABASE wizard_stat_db;
-    GRANT ALL PRIVILEGES ON DATABASE wizard_stat_db TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE wizard_stat_db TO "$POSTGRES_USER";
 EOSQL
